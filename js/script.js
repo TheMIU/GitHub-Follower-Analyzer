360cc128e34ccd89ad75c3d18831ff3a8dc2021c
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2023-11-07 19:37:52 +0530

    all followers with count

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const { Octokit } = require('@octokit/core');
+/* const { Octokit } = require('@octokit/core');
 
 const octokit = new Octokit({
     auth: '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', //  actual GitHub token
@@ -32,7 +32,53 @@ fetchFollowers(username)
     .catch((error) => {
         console.error('Error:', error);
     });
+ */
 
+//////////////////////////////////
+const { Octokit } = require('@octokit/core');
 
+const octokit = new Octokit({
+    auth: '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', // actual GitHub token
+});
 
+async function fetchAllFollowers(username) {
+    try {
+        let allFollowers = [];
+        let page = 1;
+        let response;
 
+        do {
+            response = await octokit.request('GET /users/{username}/followers', {
+                username,
+                page,
+                per_page: 100, // Adjust the per_page value as needed
+                headers: {
+                    'X-GitHub-Api-Version': '2022-11-28',
+                },
+            });
+
+            allFollowers = allFollowers.concat(response.data);
+
+            page++;
+        } while (response.headers.link && response.headers.link.includes('rel="next"'));
+
+        return allFollowers;
+
+    } catch (error) {
+        console.error('Error:', error.message);
+        return [];
+    }
+}
+
+//GitHub username
+const username = 'themiu';
+
+fetchAllFollowers(username)
+    .then((followers) => {
+        const followerNames = followers.map(follower => follower.login);
+        console.log('Followers:', followerNames.length);
+        console.log('Follower Names:', followerNames);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
