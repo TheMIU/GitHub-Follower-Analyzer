66131d1ed64273223ce5976afde37e2b2123da68
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2023-11-08 00:15:43 +0530

    updated with token and username

--- js/script.js
+++ js/script.js
@@ -1,13 +1,55 @@
+const { createTokenAuth } = require('@octokit/auth-token');
 const { Octokit } = require('@octokit/core');
 
-// Github token
 const githubToken = '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';
+const githubUsername = 'themiu';
 
-const octokit = new Octokit({
-    auth: `token ${githubToken}`,
-});
+// Create a token authentication instance
+const auth = createTokenAuth(githubToken);
 
-async function fetchAllFollowers(username) {
+// Authenticate using the token
+auth()
+    .then((authResponse) => {
+        const token = authResponse.token;
+        const octokit = new Octokit({ auth: `token ${token}` });
+
+        // Now you can use the token and octokit for authentication
+        console.log('Token:', token);
+
+        // You can call your fetch functions here with the retrieved values
+        fetchAllFollowers(githubUsername, octokit)
+            .then((followers) => {
+                
+                fetchAllFollowings(githubUsername, octokit)
+                .then((followings) => {
+                        console.log('Followers:', followers.length);
+                        console.log('Following:', followings.length);
+
+                        const followerNames = followers.map(follower => follower.login);
+                        const followingNames = followings.map(following => following.login);
+
+                        // Followers but not Following
+                        const followersNotFollowing = followerNames.filter(name => !followingNames.includes(name));
+
+                        // Following but not Followers
+                        const followingNotFollowers = followingNames.filter(name => !followerNames.includes(name));
+
+                        console.log('Followers but not Following:', followersNotFollowing);
+                        console.log('Following but not Followers:', followingNotFollowers);
+                    })
+                    .catch((error) => {
+                        console.error('Error:', error);
+                    });
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    })
+    .catch((error) => {
+        console.error('Authentication failed:', error);
+    });
+
+async function fetchAllFollowers(username, octokit) {
     try {
         let allFollowers = [];
         let page = 1;
@@ -18,9 +60,6 @@ async function fetchAllFollowers(username) {
                 username,
                 page,
                 per_page: 100, // Adjust the per_page value as needed
-                headers: {
-                    'X-GitHub-Api-Version': '2022-11-28',
-                },
             });
 
             allFollowers = allFollowers.concat(response.data);
@@ -29,14 +68,13 @@ async function fetchAllFollowers(username) {
         } while (response.headers.link && response.headers.link.includes('rel="next"'));
 
         return allFollowers;
-
     } catch (error) {
         console.error('Error:', error.message);
         return [];
     }
 }
 
-async function fetchAllFollowings(username) {
+async function fetchAllFollowings(username, octokit) {
     try {
         let allFollowings = [];
         let page = 1;
@@ -47,9 +85,6 @@ async function fetchAllFollowings(username) {
                 username,
                 page,
                 per_page: 100, // Adjust the per_page value as needed
-                headers: {
-                    'X-GitHub-Api-Version': '2022-11-28',
-                },
             });
 
             allFollowings = allFollowings.concat(response.data);
@@ -58,34 +93,8 @@ async function fetchAllFollowings(username) {
         } while (response.headers.link && response.headers.link.includes('rel="next"'));
 
         return allFollowings;
-
     } catch (error) {
         console.error('Error:', error.message);
         return [];
     }
 }
-
-// GitHub username
-const username = 'themiu';
-
-Promise.all([fetchAllFollowers(username), fetchAllFollowings(username)])
-    .then(([followers, followings]) => {
-        const followerNames = followers.map(follower => follower.login);
-        const followingNames = followings.map(following => following.login);
-
-        // Followers but not Following
-        const followersNotFollowing = followerNames.filter(name => !followingNames.includes(name));
-
-        // Following but not Followers
-        const followingNotFollowers = followingNames.filter(name => !followerNames.includes(name));
-
-        //console.log('Followers:', followerNames);
-        //console.log('Following:', followingNames);
-        console.log('Followers:', followerNames.length);
-        console.log('Following:', followingNames.length);
-        console.log('Followers but not Following:', followersNotFollowing);
-        console.log('Following but not Followers:', followingNotFollowers);
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
