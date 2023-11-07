66cb79034275747745626fb66e09529329b49fcf
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2023-11-07 19:50:51 +0530

    get followers and following done

--- js/script.js
+++ js/script.js
@@ -1,40 +1,3 @@
-/* const { Octokit } = require('@octokit/core');
-
-const octokit = new Octokit({
-    auth: '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', //  actual GitHub token
-});
-
-async function fetchFollowers(username) {
-    try {
-        const response = await octokit.request('GET /users/{username}/followers', {
-            username,
-            headers: {
-                'X-GitHub-Api-Version': '2022-11-28',
-            },
-        });
-
-        const followers = response.data;
-        return followers;
-    } catch (error) {
-        console.error('Error:', error.message);
-        return [];
-    }
-}
-
-// Replace 'github-username'
-const username = 'themiu';
-
-fetchFollowers(username)
-    .then((followers) => {
-        const followerNames = followers.map(follower => follower.login);
-        console.log('Follower Names:', followerNames);
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
- */
-
-//////////////////////////////////
 const { Octokit } = require('@octokit/core');
 
 const octokit = new Octokit({
@@ -70,6 +33,37 @@ async function fetchAllFollowers(username) {
     }
 }
 
+async function fetchAllFollowings(username) {
+    try {
+        let allFollowings = [];
+        let page = 1;
+        let response;
+
+        do {
+            response = await octokit.request('GET /users/{username}/following', { // Updated to 'following'
+                username,
+                page,
+                per_page: 100, // Adjust the per_page value as needed
+                headers: {
+                    'X-GitHub-Api-Version': '2022-11-28',
+                },
+            });
+
+            allFollowings = allFollowings.concat(response.data);
+
+            page++;
+        } while (response.headers.link && response.headers.link.includes('rel="next"'));
+
+        return allFollowings;
+    } catch (error) {
+        console.error('Error:', error.message);
+        return [];
+    }
+}
+
+
+/////////////////////////////////////////////////////////
+
 //GitHub username
 const username = 'themiu';
 
@@ -77,7 +71,17 @@ fetchAllFollowers(username)
     .then((followers) => {
         const followerNames = followers.map(follower => follower.login);
         console.log('Followers:', followerNames.length);
-        console.log('Follower Names:', followerNames);
+        //console.log('Follower Names:', followerNames);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+
+fetchAllFollowings(username)
+    .then((followings) => {``
+        const followingNames = followings.map(following => following.login);
+        console.log('followings:', followingNames.length);
+        //console.log('following Names:', followingNames);
     })
     .catch((error) => {
         console.error('Error:', error);
